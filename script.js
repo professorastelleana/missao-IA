4ed91bc8271f7315ea4321141301e98e4d4f2d19
professorastelleana <145031308+professorastelleana@users.noreply.github.com>
2024-06-17 08:31:33 -0300

    js

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+
+    "Pergunta 2"
+];
